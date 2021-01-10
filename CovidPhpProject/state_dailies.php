3da2435521a8fa818f5268a1bde97b1cd9d540a4
Gary E. Larson <gary.larson@outlook.com>
2021-01-10 12:18:34 -0600

    added rest of database tables to dev web site
    added html and php files

---
+++ CovidPhpProject/state_dailies.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Gary Larson <gary@thalic.mobi>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+    $userState = filter_input(INPUT_POST, 'state');
+    // create connection string
+    $servername = "52d9225.online-server.cloud";
+    $username = "web_php";
+    $password = 'nz3Rp"3XZL=2v4.Q';
+    $database = "covid";
+
+    try {
+        // open connection to database
+        $db_conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+        // set the PDO error mode to exception
+        $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+        if (!$db_conn) {
+            die("Could not connect");
+        }
+        
+        if ($userState == null) {
+            $state = 55;
+        } else {
+            $state = $userState;
+        }
+        
+        define ("TABLE_QUERY", "SELECT state, cases, deaths, recovered, date
+                    FROM state_dailies 
+                    INNER JOIN states 
+                    ON state_dailies.state_id = states.id
+                    WHERE state_id = :stateParam
+                    ORDER BY date DESC");
+        
+        // get results from the table query
+        $stmt = $db_conn->prepare(TABLE_QUERY);
+        // add parameter
+        $stmt->bindParam(':stateParam', $state);
+        $stmt->execute();
+        // set the resulting array to associative
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        if (!$stmt) {
+            die("No Table Results");
+        }
+      // Build table body
+      while ($row = $stmt->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $row["state"] . "</td>";
+        echo "<td>" . number_format(intval($row["cases"])) . "</td>";
+        echo "<td>" . number_format(intval($row["deaths"])) . "</td>"; 
+        echo "<td>" . number_format(intval($row["recovered"])) . "</td>";
+        echo "<td>" . $row["date"] . "</td>";
+        echo "</tr>";
+      }
+      // close database connection
+      $db_conn = null;
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
---
+++ CovidPhpProject/state_dailies_states.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Gary Larson <gary@thalic.mobi>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+    // create connection string
+    $servername = "52d9225.online-server.cloud";
+    $username = "web_php";
+    $password = 'nz3Rp"3XZL=2v4.Q';
+    $database = "covid";
+
+    try {
+        // open connection to database
+        $db_conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+        // set the PDO error mode to exception
+        $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+        if (!$db_conn) {
+            die("Could not connect");
+        }
+        
+        define ("STATE_QUERY", "SELECT DISTINCT state, state_id "
+                . "FROM state_dailies "
+                . "INNER JOIN states ON state_dailies.state_id = "
+                . "states.id "
+                . "ORDER BY state;");
+        
+        // get results from the date query
+        $stmt = $db_conn->prepare(STATE_QUERY);
+        $stmt->execute();
+        // set the results to associative
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        if(!$stmt) {
+            die("No State Results");
+        }
+        
+        // Build ui list
+        while ($row = $stmt->fetch()) {
+            echo '<li><a href="#" onclick=loadTable("' . $row["state_id"] . '");>' . 
+                    $row["state"] . '</a>';
+        }
+      // close database connection
+      $db_conn = null;
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
